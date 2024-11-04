9854ab25f6f9e88df007c7dc95059b11c6ca6c5e
Regivaldo Sousa <regivaldo.ferreira@ifsp.edu.br>
2024-11-04 17:37:37 -0300

    ajustes dos dias

--- index.py
+++ index.py
@@ -24,7 +24,17 @@ RPF = 0.6
 st.title("Simulação de Crescimento da Planta Forrageira")
 
 # Slider para entrada do usuário com atualização em tempo real
-duracao = st.slider("Digite o tempo de análise em dias:", min_value=1, max_value=10000, value=100, step=1)
+tempo_dias = st.slider("Digite o tempo de análise em dias:", min_value=1, max_value=10000, value=100, step=1)
+
+# Conversão para anos, meses e semanas
+anos = tempo_dias // 365
+meses = (tempo_dias % 365) // 30
+semanas = ((tempo_dias % 365) % 30) // 7
+dias_restantes = ((tempo_dias % 365) % 30) % 7
+
+# Exibindo o valor inserido e as conversões
+st.write("Valor de tempo inserido:", tempo_dias, "dias")
+st.write(f"Isso equivale a: {anos} anos, {meses} meses, {semanas} semanas e {dias_restantes} dias.")
 
 # Cálculo dos parâmetros de crescimento
 TCC = calcular_TCC(TAL, IAF)
@@ -32,7 +42,7 @@ RAF = calcular_RAF(AFE, RPF)
 TCR = calcular_TCR(TAL, RAF)
 
 # Cálculo da biomassa acumulada
-tempo = np.arange(0, duracao + 1)
+tempo = np.arange(0, tempo_dias + 1)
 biomassa = np.zeros(len(tempo))
 biomassa[0] = 1  # Biomassa inicial
 
