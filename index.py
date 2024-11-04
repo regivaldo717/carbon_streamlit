12d499673a5e49cc51936869739ee80c1751c89d
Regivaldo Sousa <regivaldo.ferreira@ifsp.edu.br>
2024-11-04 18:03:11 -0300

    ajuste título

--- index.py
+++ index.py
@@ -20,41 +20,32 @@ IAF = 0.8
 AFE = 0.2
 RPF = 0.6
 
-# Título do aplicativo
-st.title("Simulação de Crescimento da Planta Forrageira")
+st.title("Simulação do Crescimento da Vegetação Forrageira")
 
-# Slider para entrada do usuário com atualização em tempo real
 tempo_dias = st.slider("Digite o tempo de análise em dias:", min_value=1, max_value=10000, value=100, step=1)
 
-# Conversão para anos, meses e semanas
 anos = tempo_dias // 365
 meses = (tempo_dias % 365) // 30
 semanas = ((tempo_dias % 365) % 30) // 7
 dias_restantes = ((tempo_dias % 365) % 30) % 7
 
-# Exibindo o valor inserido e as conversões
 st.write("Valor de tempo inserido:", tempo_dias, "dias")
 st.write(f"Isso equivale a: {anos} anos, {meses} meses, {semanas} semanas e {dias_restantes} dias.")
 
-# Cálculo dos parâmetros de crescimento
 TCC = calcular_TCC(TAL, IAF)
 RAF = calcular_RAF(AFE, RPF)
 TCR = calcular_TCR(TAL, RAF)
 
-# Cálculo da biomassa acumulada
 tempo = np.arange(0, tempo_dias + 1)
 biomassa = np.zeros(len(tempo))
-biomassa[0] = 1  # Biomassa inicial
-
+biomassa[0] = 1  
 for i in range(1, len(tempo)):
     biomassa[i] = biomassa[i-1] * np.exp(TCR)
 
-# Criação do gráfico
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=tempo, y=biomassa, mode='lines', name='Biomassa'))
 fig.update_layout(title="Crescimento da Planta Forrageira",
                   xaxis_title="Tempo (dias)",
                   yaxis_title="Biomassa acumulada")
 
-# Exibição do gráfico no Streamlit
 st.plotly_chart(fig)
